ea696237a41bcebeed216b54f95ecb7a72c33ee1
rohits2509 <rohits2509@gmail.com>
2017-02-26 23:51:19 -0800

    additional files

---
+++ assests/js/dogClick.js
@@ -0,0 +1,18 @@
+var s1core = 0
+var s2core = 0
+
+$("#img1").on("click", function(){
+	s1core++;
+	
+	$("#s1core").text(s1core);
+
+});
+
+
+
+$("#img2").on("click", function(){
+	s2core++;
+	
+	$("#s2core").text(s2core);
+
+});
